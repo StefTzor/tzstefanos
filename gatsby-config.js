3c2ded7eb6114e58e25fa65a67089bf3929313cb
StefTzor <34420752+StefTzor@users.noreply.github.com>
2020-10-13 23:33:48 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,8 +26,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
+        name: `Stefanos Tzortzoglou`,
+        short_name: `steftzor`,
         description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
         start_url: `/`,
         background_color: `#141821`,
