31365fe3ca0d39046434db358d2a68dee9a48bd1
StefTzor <34420752+StefTzor@users.noreply.github.com>
2020-10-14 23:11:13 +0300

    Rebuild

---
+++ src/@lekoarts/gatsby-theme-cara/components/project-card.tsx
@@ -0,0 +1,57 @@
+/** @jsx jsx */
+import React from "react"
+import { jsx } from "theme-ui"
+
+type ProjectCardProps = {
+  link: string
+  title: string
+  children: React.ReactNode
+  bg: string
+  img: any
+}
+
+const ProjectCard = ({ link, title, children, bg, img  }: ProjectCardProps) => (
+  <a
+    href={link}
+    target="_blank"
+    rel="noreferrer noopener"
+    sx={{
+      width: `50%`,
+      boxShadow: `lg`,
+      position: `relative`,
+      textDecoration: `none`,
+      borderRadius: `lg`,
+      px: 4,
+      py: [4, 5],
+      color: `white`,
+      background: bg || `none`,
+      transition: `all 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275) !important`,
+      "&:hover": {
+        color: `white !important`,
+        transform: `translateY(-5px)`,
+        boxShadow: `xl`,
+      },
+    }}
+  >
+    <div sx={{ opacity: 0.85, textShadow: `0 2px 10px rgba(0, 0, 0, 0.3)` }}>{children}</div>
+    <div
+      sx={{
+        textTransform: `none`,
+        letterSpacing: `wide`,
+        pt: 4,
+        fontSize: [4, 5],
+        fontWeight: `medium`,
+        lineHeight: 1,
+      }}
+    >
+      {title}
+    </div>
+    <div>
+      <img src={img} />
+      {console.log(img)}
+    </div>
+  </a>
+  
+)
+
+export default ProjectCard
